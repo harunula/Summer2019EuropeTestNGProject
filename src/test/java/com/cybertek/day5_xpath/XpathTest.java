9f284ce07483b056d7d65a07daa78a81005f3f06
harun <harunula@gmail.com>
2019-11-10 22:07:36 +0100

    Day_5

---
+++ src/test/java/com/cybertek/day5_xpath/XpathTest.java
@@ -0,0 +1,22 @@
+package com.cybertek.day5_xpath;
+
+import com.cybertek.utilities.WebDriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class XpathTest {
+    public static void main(String[] args) {
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("http://www.amazon.com");
+        WebElement searchbox = driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]"));
+        searchbox.sendKeys("selenium");
+        WebElement submitButton = driver.findElement(By.xpath("//input[@value='Go']"));
+        submitButton.click();
+
+
+
+    }
+}
---
+++ src/test/java/com/cybertek/day5_xpath/amazon.java
@@ -0,0 +1,33 @@
+package com.cybertek.day5_xpath;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class amazon {
+    /**
+     * navigate to amazon.com
+     * type selenium in the searchbox
+     * click search button
+     */
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://www.amazon.com");
+        //locate searchbox
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        //type selenium
+        searchBox.sendKeys("Selenium");
+        Thread.sleep(1000);
+        //locate searchButton
+        WebElement searchButton = driver.findElement(By.xpath("//input[@value='Go']"));
+        searchButton.click();
+
+        //WebElement resultText = driver.findElement(By.xpath("(//span[@dir='auto'][1])[1]"));
+        WebElement resultText = driver.findElement(By.xpath(" //span[contains(text(),'results for')] "));
+        System.out.println(resultText.getText());
+
+    }
+
+}
